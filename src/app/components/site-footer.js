83d3dd95adc179b8e3ec2ee5855c5da2155161b8
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2025-03-02 22:40:57 +0100

    (fix): change footer

--- src/app/components/site-footer.js
+++ src/app/components/site-footer.js
@@ -5,7 +5,7 @@ export function SiteFooter() {
     <footer className="border-t border-border/40 py-8 bg-card">
       <div className="container px-4 flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-center text-sm text-muted-foreground md:text-left">
-          Built with passion by John Doe
+          Built using Next15
         </p>
         <div className="flex items-center space-x-6">
           <a
